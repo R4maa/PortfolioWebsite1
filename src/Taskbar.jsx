a08b70dc9510b9e8d3b4271f1a0ab4321b98b3ac
R4maa <amarrazzaq1@gmail.com>
2024-10-07 21:48:10 -0400

    Image in start button
    issue with alignment, commented out lines in css. when uncommented clock disappers, look into css grid and css flexbox

--- src/Taskbar.jsx
+++ src/Taskbar.jsx
@@ -14,13 +14,17 @@ function Taskbar(){
 
 export default Taskbar
 
-
+// TODO: Add startbutton functionality 
 function StartButton(){
     
     return(
-        <button className = 'startButton'>Start</button>
+        <button className = 'startButton'>
+            <img className="resizeStartImg" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/94ff4db7-7a6a-4aa4-8d78-3a2aaa5bd6c4/ddir3zk-9146cc22-085f-4638-92d8-011e03cdf085.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk0ZmY0ZGI3LTdhNmEtNGFhNC04ZDc4LTNhMmFhYTViZDZjNFwvZGRpcjN6ay05MTQ2Y2MyMi0wODVmLTQ2MzgtOTJkOC0wMTFlMDNjZGYwODUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.1bMTDRN3t9f2G9yTLWTxImNfg-r3VJEaOqHb1K88Fi4"></img>
+            <h2>Start</h2>
+        </button>
     );
 }
+
 
 function Clock(){
     let time  = new Date().toLocaleTimeString()
@@ -31,9 +35,7 @@ function Clock(){
       setTime(time)
     }
     setInterval(UpdateTime)
-  
-    
-
+      
     return(
         <div className='clock'>
             <p>{ctime}</p>
